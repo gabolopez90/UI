e52e479b3ae444f4ffec8ce8f665c33f3df0d337
Gabriel Lopez <gabolopez1990@gmail.com>
2023-03-23 08:44:18 -0400

    Agregar archivos de proyecto.

---
+++ ViewModels/ConsultaModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.ViewModels
+{
+    public class ConsultaModel : ViewModelBase
+    {
+
+        public string[] Nacionalidad { get; set; } = {
+                "V",
+                "E",
+                "J",
+                "P",
+                "M"
+            };
+
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using FontAwesome.Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UI.ViewModels
+{
+    public class MainViewModel:ViewModelBase
+    {
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+        
+        //Propiedades
+        public ViewModelBase CurrentChildView { 
+            get => _currentChildView; 
+            set { 
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            } 
+        }
+        public string Caption
+        {
+            get => _caption;
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+        public IconChar Icon
+        {
+            get => _icon;
+            set { 
+                _icon = value; 
+                OnPropertyChanged(nameof(Icon));
+            } 
+        }
+
+        public ICommand ShowInicioCommand { get; set; }
+        public ICommand ShowConsultaCommand { get; set; }
+        public ICommand ShowInformeCommand { get; set; }
+
+        public MainViewModel()
+        {
+            ShowInicioCommand = new ViewModelCommand(ExecuteShowInicioCommand);
+            ShowConsultaCommand = new ViewModelCommand(ExecuteShowConsultaCommand);
+            ShowInformeCommand = new ViewModelCommand(ExecuteShowInformeCommand);
+
+            ExecuteShowInicioCommand(obj: null);
+        }
+
+        private void ExecuteShowInicioCommand(object? obj)
+        {
+            CurrentChildView = new InicioModel();
+            Caption = "Inicio";
+            Icon = IconChar.Home;
+        }
+
+        private void ExecuteShowConsultaCommand(object obj)
+        {
+            CurrentChildView = new ConsultaModel();
+            Caption = "Consulta";
+            Icon = IconChar.Search;
+        }
+
+        private void ExecuteShowInformeCommand(object obj)
+        {
+            CurrentChildView = new InformeModel();
+            Caption = "Informe";
+            Icon = IconChar.File;
+        }
+    }
+}
